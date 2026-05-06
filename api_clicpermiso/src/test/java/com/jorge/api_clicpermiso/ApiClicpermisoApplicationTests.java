63a31be2143ecea31173ca9541d65d5cce024000
yoryi404 <jalegrem04@iesalbarregas.es>
2026-05-06 16:50:29 +0200

    feat: Añadir todo lo generado con spring initializr

---
+++ api_clicpermiso/src/test/java/com/jorge/api_clicpermiso/ApiClicpermisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.api_clicpermiso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClicpermisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
